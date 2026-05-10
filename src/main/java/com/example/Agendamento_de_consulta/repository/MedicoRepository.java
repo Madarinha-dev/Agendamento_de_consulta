166745dc84509406298cac361d5c70b7c543d42d
Randerson Barros de Albuquerque <83302116+Randersonbarros@users.noreply.github.com>
2026-05-10 01:31:03 -0300

    feat: add query method findByEspecialidade to MedicoRepository
    - Implementação de Query Method personalizado no MedicoRepository.
    
    - Adição do método findByEspecialidade(String especialidade) para permitir a filtragem de médicos por área de atuação.
    
    - Atendimento direto ao Requisito Funcional RF14 (Buscar Médico por Especialidade) do escopo do projeto.
    
    - Mantém o código expressivo e curto, delegando a interpretação da consulta ao Spring Data JPA.

--- src/main/java/com/example/Agendamento_de_consulta/repository/MedicoRepository.java
+++ src/main/java/com/example/Agendamento_de_consulta/repository/MedicoRepository.java
@@ -1,11 +1,16 @@
 package com.example.Agendamento_de_consulta.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.Agendamento_de_consulta.entity.Especialidade;
 import com.example.Agendamento_de_consulta.entity.Medico;
+
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
     
+    List<Medico> findByEspecialidades(List<Especialidade> especialidades);
 }
