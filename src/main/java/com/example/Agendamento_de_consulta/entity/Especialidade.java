de519efde36a804c842ed1fbe97ecafd57803a3b
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2026-05-09 22:51:28 -0300

    feat: add Especialidade entity
    Criação da entidade Especialidade com mapeamento JPA e campos ID, Nome e CBO conforme modelagem de dados.

--- src/main/java/com/example/Agendamento_de_consulta/entity/Especialidade.java
+++ src/main/java/com/example/Agendamento_de_consulta/entity/Especialidade.java
@@ -1,5 +1,26 @@
 package com.example.Agendamento_de_consulta.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "especialidades")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Especialidade {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da especialidade é obritgatório")
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "codigo_cbo")
+    private String codigoCbo;
 }
