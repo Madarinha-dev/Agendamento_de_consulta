12512c545b7dea88d9549cfa923407faccf42dc9
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2026-05-17 00:12:00 -0300

    refactor: align Usuario entity with project requirements
    - Mapeamento completo e padronização dos tipos de dados do item 06 da modelagem (Usuarios).
    - Adicionado suporte ao campo confirmacaoSenha mapeado como @Transient.
    - Implementação de regras do RNF02 adicionando validações de tamanho (@Size) e obrigatoriedade (@NotBlank, @Email).
    - Configuração de restrições de unicidade (unique = true) para CPF e Email diretamente no banco de dados.
    - Ajuste de nomenclatura de colunas físicas de camelCase para snake_case no Hibernate.

--- src/main/java/com/example/Agendamento_de_consulta/entity/Usuario.java
+++ src/main/java/com/example/Agendamento_de_consulta/entity/Usuario.java
@@ -1,8 +1,7 @@
 package com.example.Agendamento_de_consulta.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -14,27 +13,46 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Usuario {
 
+    // COLUNAS SOBRE USUARIO;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 150, message = "O nome não pode ter mais de 150 caracteres")
+    @Column(nullable = false, length = 150)
     private String nome;
 
-    @Email(message = "Email inválido")
     @NotBlank(message = "O email é obrigatório")
-    @Column(unique = true)
+    @Email(message = "Email inválido")
+    @Size(max = 100, message = "O email não pode ter mais de 100 caracteres")
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
 
+
+
+    @Size(max = 100, message = "A profissão não pode ter mais de 100 caracteres")
+    @Column(length = 100)
     private String profissao;
 
     @NotBlank(message = "O CPF é obrigatório")
-    @Column(unique = true)
+    @Size(min = 11, max = 14, message = "O CPF deve conter entre 11 e 14 caracteres")
+    @Column(nullable = false, unique = true, length = 14)
     private String cpf;
 
     @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, max = 255, message = "A senha deve ter entre 6 e 255 caracteres")
+    @Column(nullable = false)
     private String senha;
 
-    // Campo para definir o nível de acesso (Admin, Recepcionista, etc)
+
+    // Anotação para n persistir no banco de dados, não criar a coluna, para ser ignorado no mapeamento;
+    @Transient 
+    private String confirmacaoSenha;
+
+    @NotBlank(message = "As permissões de acesso são obrigatórias")
+    @Size(max = 50, message = "As permissões de acesso não podem ter mais de 50 caracteres")
+    @Column(name = "permissoes_acesso", nullable = false, length = 50)
     private String permissoesAcesso;
+
 }
