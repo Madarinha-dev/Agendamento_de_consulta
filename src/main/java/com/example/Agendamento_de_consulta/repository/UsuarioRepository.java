100ef2f65bf96c3555a8139f980e4aa09c14c880
Fhellype Vinicius de Paula <fhellypeviniciusdepaula96@gmail.com>
2026-05-18 14:53:17 -0300

    refactor: add query methods and comments to UsuarioRepository
    - Criados os métodos existsByCpf e existsByEmailIgnoreCase para dar suporte às regras de validação de duplicidade.
    - Adicionados comentários detalhados explicativos em cada método para auxiliar na apresentação e defesa do projeto.

--- src/main/java/com/example/Agendamento_de_consulta/repository/UsuarioRepository.java
+++ src/main/java/com/example/Agendamento_de_consulta/repository/UsuarioRepository.java
@@ -1,15 +1,20 @@
 package com.example.Agendamento_de_consulta.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.Agendamento_de_consulta.entity.Usuario;
 
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    // BUSCA O USUÁRIO PELO EMAIL, RETORNANDO UM OPCIONAL RETONANDO O USUÁRIO (CASO EXISTA) OU TA VAZIO
     Optional<Usuario> findByEmail(String email);
+
+    // BUSCA O USUÁRIO PELO CPF, retornando true ou false;
+    boolean existsByCpf(String cpf);
+ 
+    // BUSA O USUÁRIO PELO EMAIL, retornando true ou false;
+    boolean existsByEmailIgnoreCase(String email);
 }
